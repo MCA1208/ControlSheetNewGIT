ce809eb13a2d0721a73aaa8e980e39b37ff886da
MCA1208 <milton.amado10@gmail.com>
2019-08-14 06:53:03 -0300

    Inicio PaperBag

---
+++ ControlSheet/Controllers/PaperBag/PaperBagController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControlSheet.Controllers.PaperBag
+{
+    public class PaperBagController : Controller
+    {
+        // GET: PaperBag
+        public ActionResult Principal()
+        {
+            return View();
+        }
+
+
+    }
+}
