e02c2566e82b5fbeda8a1ec54287b9d12fdf4289
MCA1208 <milton.amado10@gmail.com>
2019-08-15 07:00:49 -0300

    image

---
+++ ControlSheet/Scripts/Controller/PaperBag/PrincipalController.js
@@ -0,0 +1,34 @@
+﻿
+$(document).ready(function () {
+
+    
+
+    $('#file-perfil').change(function (e) {
+        addImage(e);
+    });
+
+    function addImage(e) {
+        var file = e.target.files[0],
+            imageType = /image.*/;
+
+        if (!file.type.match(imageType))
+            return;
+
+        var reader = new FileReader();
+        reader.onload = fileOnload;
+        reader.readAsDataURL(file);
+    }
+
+    function fileOnload(e) {
+        var result = e.target.result;
+        $('#imgSalida').attr("src", result);
+    }
+
+    $('#btnModal').on("click", function () {
+
+        $('#tblModify').DataTable();
+    });
+
+    
+
+});
